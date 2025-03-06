e9b5d44184db9bd7db8949a1f5569eb85f7edec6
Rafael <rafael.zink@unidavi.edu.br>
2025-03-06 08:38:34 -0300

    oo

--- Ex3/Index/index.php
+++ Ex3/Index/index.php
@@ -3,8 +3,37 @@ require_once 'C:\xampp\htdocs\Programa-o-Web2\Ex3\vendor\autoload.php';
 
 use Dompdf\Dompdf;
 
-$dompdf = new Dompdf();
+class PDFGenerator {
+    private $dompdf;
 
+    public function __construct() {
+        $this->dompdf = new Dompdf();
+    }
+
+    /**
+     * Gera e exibe o PDF.
+     *
+     * @param string $html Conteúdo HTML que será convertido em PDF.
+     * @param string $fileName Nome do arquivo PDF.
+     * @param string $paper Tamanho do papel (ex: A4).
+     * @param string $orientation Orientação do papel (portrait ou landscape).
+     * @param bool $stream Se verdadeiro, o PDF será enviado para o navegador; caso contrário, retorna o conteúdo.
+     * @return string|void
+     */
+    public function generatePDF($html, $fileName = "exemplo.pdf", $paper = "A4", $orientation = "portrait", $stream = true) {
+        $this->dompdf->loadHtml($html);
+        $this->dompdf->setPaper($paper, $orientation);
+        $this->dompdf->render();
+
+        if ($stream) {
+            $this->dompdf->stream($fileName, ["Attachment" => false]);
+        } else {
+            return $this->dompdf->output();
+        }
+    }
+}
+
+// Conteúdo HTML
 $html = '
 <!DOCTYPE html>
 <html>
@@ -19,11 +48,6 @@ $html = '
 </html>
 ';
 
-$dompdf->loadHtml($html);
-
-$dompdf->setPaper('A4', 'portrait');
-
-$dompdf->render();
-
-$dompdf->stream("exemplo.pdf", ["Attachment" => false]);
+$pdfGenerator = new PDFGenerator();
+$pdfGenerator->generatePDF($html);
 ?>
